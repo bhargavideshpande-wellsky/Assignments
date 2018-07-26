8c2c0d8bd4bd17d42e9b7f7edce04997da2ae8c0
tavisca-bhargavi <41355447+tavisca-bhargavi@users.noreply.github.com>
2018-07-26 17:58:40 +0530

    Add files via upload

---
+++ Assignment3/Assignment3/EmployeeDetails.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class EmployeeDetails
+    {
+        public int id;
+        public string qualification, name;
+        
+        public void Emp() {
+            
+        }
+
+       public void display()
+        {
+            Console.WriteLine("EmployeeId:"+id);
+            Console.WriteLine("EmployeeName:" + name);
+            Console.WriteLine( "EmployeeQualification:"+qualification );
+            if(qualification=="BE"|| qualification == "BCA"||qualification == "BSC")
+                Console.WriteLine("Employee under IT department");
+            else if (qualification == "BCom" || qualification == "MCom" || qualification == "CA")
+                Console.WriteLine("Employee under Accounts department");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Assignment3/Assignment3/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+using System.Threading.Tasks;
+
+
+namespace Assignment3
+{
+    class NullException : Exception{
+        public NullException(string message) : base(message)
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directory = "C://Users//HP//Desktop//logs.txt";
+
+             EmployeeDetails e = new EmployeeDetails();
+            List<EmployeeDetails> emp = new List<EmployeeDetails>();
+            while(true)
+            {
+                Console.WriteLine("1. Enter details \n 2.Stop");
+                int i = int.Parse(Console.ReadLine());
+                switch (i)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Enter employee Id:");
+                            e.id = int.Parse(Console.ReadLine());
+                            try
+                            {
+                                Console.WriteLine("Enter employee Qualification:");
+                                e.qualification = Console.ReadLine();
+                                if (string.IsNullOrEmpty(e.qualification))
+                                {
+                                    throw new NullException("Please enter your qualification");
+                                    
+                                }
+                                
+                            }
+                            catch (NullException empty)
+                            {
+                                Console.WriteLine("Exception occured");
+                                Console.WriteLine(empty.Message);
+                                //if (File.Exists(directory))
+                                //{
+                                try
+                                {
+                                    FileStream fs = new FileStream(directory, FileMode.Append);
+                                    using (StreamWriter writer = new StreamWriter(fs))
+                                    {
+                                        writer.WriteLine(empty.Message);
+                                    }
+                                } catch(Exception ex)
+                                {
+
+                                }
+                                    
+                                //}
+                            }
+                            
+                            Console.WriteLine("Enter employee Name:");
+                            e.name = Console.ReadLine();
+                            emp.Add(e);
+                            e.display();
+                            break;
+                        }
+                    case 2:
+                        {
+                            Environment.Exit(0);
+                            break;
+                        }
+                }  
+            }
+            
+           
+        }
+    }
+
+    
+}
